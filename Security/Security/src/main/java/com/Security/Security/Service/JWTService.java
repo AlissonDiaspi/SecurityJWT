60e25ee48733aa6af828e3bcb9b4ed4991677b18
AlissonDiaspi <Alissondias7@outlook.com>
2025-12-30 19:10:44 -0300

    finalização do projeto

--- Security/Security/src/main/java/com/Security/Security/Service/JWTService.java
+++ Security/Security/src/main/java/com/Security/Security/Service/JWTService.java
@@ -6,7 +6,8 @@ import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
-import lombok.Value;
+import jakarta.validation.Valid;
+
 import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
@@ -15,63 +16,57 @@ import java.security.Key;
 import java.util.Date;
 import java.util.Set;
 import java.util.function.Function;
+import org.springframework.beans.factory.annotation.Value;
 
 @Service
-public class JWTService { //  serviço para manipulação de tokens JWT
+public class JWTService {
+
+    @Value("${jwt.secret}")
+    private String secretKey;
 
 
-    private static final String SECRET_KEY =
-            "minha-chave-secreta-projeto-JWT-Security-login-register-logout-refresh-token"; // chave secreta
-
-    //  1 hora
     private static final long EXPIRATION_TIME = 1000 * 60 * 60;
 
-
     private Key getSignKey() {
-        return Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+        return Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
     }
-
 
     public String generateToken(String email, Set<String> roles) {
         return Jwts.builder()
-                .setSubject(email) //  subject = identidade do usuário
+                .setSubject(email)
                 .setIssuedAt(new Date(System.currentTimeMillis()))
                 .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
                 .signWith(getSignKey(), SignatureAlgorithm.HS256)
                 .compact();
     }
 
-
-    public String extractUsername(String token) { // extrai email do token
+    public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
     }
-
 
     public <T> T extractClaim(String token, Function<Claims, T> resolver) {
         final Claims claims = extractAllClaims(token);
         return resolver.apply(claims);
     }
 
-
     private Claims extractAllClaims(String token) {
         return Jwts.parserBuilder()
-                .setSigningKey(getSignKey()) //  NÃO verifyWith
+                .setSigningKey(getSignKey())
                 .build()
                 .parseClaimsJws(token)
                 .getBody();
     }
-
 
     private boolean isTokenExpired(String token) {
         return extractClaim(token, Claims::getExpiration)
                 .before(new Date());
     }
 
-
-    public boolean isTokenValid(String token, String userEmail) { // validar token
+    public boolean isTokenValid(String token, String userEmail) {
         final String username = extractUsername(token);
         return username.equals(userEmail) && !isTokenExpired(token);
     }
+
     public String extractUsernameEvenIfExpired(String token) {
         try {
             return extractUsername(token);
@@ -79,5 +74,4 @@ public class JWTService { //  serviço para manipulação de tokens JWT
             return e.getClaims().getSubject();
         }
     }
-
 }
